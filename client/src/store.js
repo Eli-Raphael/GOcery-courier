a0664878d2a92f1880664a4e1adc92bcb960c06d
Haze2670 <raymarktomas2670@gmail.com>
2024-05-05 23:50:09 +0800

    5/5/24
    download redux tool to work
    
    using extension in chrome

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,19 @@
-import {combineReducers} from 'redux'
-import {createStore, applyMiddleware} from 'redux'
-import { thunk } from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import { getAllProductsReducer } from './reducers/productsReducers'
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { getAllProductsReducer } from './reducers/productsReducers';
+
 const finalReducer = combineReducers({
-    getAllProductsReducer: getAllProductsReducer
-})
+    allProducts: getAllProductsReducer
+});
 
-const initialState = {}
-const composeEnhancers = composeWithDevTools({});
+const initialState = {};
+const middleware = [thunk];
 
-const store = createStore(finalReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(
+    finalReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
